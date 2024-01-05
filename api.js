801dcc6fa88875c42415d975fe54164463099e01
rororyo <ryonathaniel05@gmail.com>
2024-01-05 17:55:40 +0700

    added post functionalities, get all posts from the db

--- api.js
+++ api.js
@@ -1,10 +1,64 @@
-//imports
-import express from 'express'
+// imports
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+import multer from "multer";
 
-const app= express()
-app.use(express.static('public'))
-const port=4000
+const { Client } = pg;
 
 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port,()=>{console.log(`api is running on port ${port}`)})
+
+app.use(express.static("public"));
+const port = 4000;
+
+const client = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Simple Blog",
+  password: "Ryokeren123",
+  port: 5432,
+});
+
+await client.connect();
+
+// get request -> display 5 posts
+app.get("/posts", async (req, res) => {
+  const result = await client.query(
+    "SELECT * FROM posts ORDER BY date_created DESC LIMIT 5"
+  );
+
+  const posts = result.rows;
+  res.send(posts);
+});
+
+// post new blog
+app.post('/posts', async (req, res) => {
+    
+  try {
+    const post = {
+      category: req.body.category,
+      title: req.body.title,
+      post_content: req.body.post_content,
+      author: req.body.author,
+      picture: req.body.picture
+    }
+
+    // Insert the data into the database
+    const result = await client.query('INSERT INTO posts(category, title, post_content, author, picture) VALUES($1, $2, $3, $4, $5)',
+      [post.category, post.title, post.post_content, post.author, post.picture]);
+
+    res.status(200).send('Post added successfully.');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+
+});
+
+app.listen(port, () => {
+  console.log(`api is running on port ${port}`);
+});
--- index.js
+++ index.js
@@ -1,13 +1,58 @@
 //imports
-import express from 'express'
+import express from 'express';
+import axios from 'axios';
+import multer from 'multer';
+import bodyParser from "body-parser";
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = 3000;
+const apiUrl = 'http://localhost:4000';
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + file.originalname);
+    },
+});
+const upload = multer({ storage: storage });
+app.use(upload.single('picture'));
 
-const app= express()
-app.use(express.static('public'))
-const port=3000
-//render homepage
-app.get("/",(req,res)=>{
-    res.render('home.ejs')
+// render homepage
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get(`${apiUrl}/posts`);
+        const posts = result.data;
+        res.render('home.ejs', { posts: posts });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// render newpost page
+app.get("/newpost", async (req, res) => {
+    res.render('edit.ejs');
 })
 
+// Create a new post
+app.post("/api/posts", async (req, res) => {
+    try {
+        // Include req.file.filename in the data sent to the API
+        const postData = {
+            ...req.body,
+            picture: req.file.filename,
+        };
 
-app.listen(port,()=>{console.log(`server is running on port ${port}`)})
+        // Make the POST request to your API with modified data
+        await axios.post(`${apiUrl}/posts`, postData);
+        res.redirect("/");
+    } catch (error) {
+        console.error("Error creating post:", error.message);
+        res.status(500).json({ message: "Error creating post" });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
